8f44029fd55c9aba7ce0541bcbdf41f95dbc72ab
sowndaryasree <sowndaryasree2007@gmail.com>
2026-03-07 16:55:27 +0530

    setup

--- backend/database.py
+++ backend/database.py
@@ -2,8 +2,8 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-DATABASE_URL = "postgresql://postgres:postgres@localhost:5432/transport_db"
-
+DATABASE_URL = "postgresql://postgres:postgres@db.pqkuncbhligypafaodgn.supabase.co:5432/postgres"
+engine = create_engine(DATABASE_URL, pool_pre_ping=True)
 engine = create_engine(DATABASE_URL)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
