d68886429e31eaacbe0e1b69eb99468f053449f3
sowndaryasree <sowndaryasree2007@gmail.com>
2026-03-08 10:00:34 +0530

    fix imports for render

--- backend/models.py
+++ backend/models.py
@@ -130,7 +130,7 @@ class Salary(Base):
     notes = Column(String)
 
 from sqlalchemy import Column, Integer, String, Date
-from .database import Base
+from database import Base
 
 
 class Attendance(Base):
