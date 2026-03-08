5a1071292251221c8703dab7672fa8238ef762ef
sowndaryasree <sowndaryasree2007@gmail.com>
2026-03-08 20:50:16 +0530

    changes done

--- backend/database.py
+++ backend/database.py
@@ -9,3 +9,9 @@ engine = create_engine(DATABASE_URL,pool_pre_ping=True)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
